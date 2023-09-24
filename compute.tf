f2b3cc5d4e73af791c9268beefedcebabc9bd9f1
Yannis Bouhamou <61419311+Yanka13@users.noreply.github.com>
2023-09-24 23:54:49 +0200

    Update compute.tf

--- compute.tf
+++ compute.tf
@@ -5,14 +5,12 @@ resource "azurerm_linux_virtual_machine" "vm" {
   location            = var.location
   size                = "Standard_B1s"
   admin_username      = "adminuser"
+  admin_password      = "password321!"
   network_interface_ids = [
     azurerm_network_interface.nic.id,
   ]
 
-  admin_ssh_key {
-    username   = "adminuser"
-    public_key =  file("~/.ssh/id_rsa.pub") # Ensure you have the public key at this location
-  }
+
 
   custom_data = base64encode(file("${path.module}/config.sh"))
 
