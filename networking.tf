6876060e34657b77f750ebdc5c8fa96b3ecb6341
Yannis Bouhamou <61419311+Yanka13@users.noreply.github.com>
2024-03-08 21:33:25 +0100

    Update networking.tf

--- networking.tf
+++ networking.tf
@@ -4,7 +4,6 @@ resource "azurerm_virtual_network" "vnet" {
   name                = "example-vnet"
   address_space       = ["10.0.0.0/16"]
   location            = var.location
-  resource_group_name = var.resource_group_name
 }
 
 # 2 - Subnet in Vnet
